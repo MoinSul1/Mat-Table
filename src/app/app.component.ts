226d80cb4e804149dade06cc759902f48b243dab
MoinSul1 <moinsul.zversal@gmail.com>
2021-02-15 11:59:01 +0530

    mat table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'practise1';
+  
+   
+  constructor(   ) {
+      
+  }
+  ngOnInit(){
+    
+    
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewcompComponent } from './newcomp/newcomp.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { UsertableComponent } from './components/usertable/usertable.component';
+import { MatTableModule } from '@angular/material/table';
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewcompComponent,
+    FirstComponent,
+    SecondComponent,
+    UsertableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
